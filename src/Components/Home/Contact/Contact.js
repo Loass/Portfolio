89a1a8bef899589af9c1f5d14532a91332c8b0d5
Lois Bonsignore <bonsignore.lois@gmail.com>
2021-12-09 17:12:35 +0100

    contact

--- src/Components/Home/Contact/Contact.js
+++ src/Components/Home/Contact/Contact.js
@@ -1,16 +1,33 @@
-import React from "react";
-
 import "./Contact.css";
-
 import send from "../../../assets/send.svg";
 import phone from "../../../assets/phone.svg";
 import mail from "../../../assets/envelope.svg";
 import github from "../../../assets/github.svg";
 import linkedin from "../../../assets/linkedin.svg";
-
 import cv from "../../../assets/CV_Bonsignore_Lois.pdf";
 
+import React, { useState } from "react";
+const axios = require('axios')
+
+
+
 function Contact() {
+  const [contactInfo, setContactInfo] = useState({
+    lastname: "",
+    firstname: "",
+    email: "",
+    message: "",
+  });
+
+
+const url = "http://localhost:9000/contact"
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.target.reset();
+    axios.post(url, contactInfo).then((res) =>   console.log(res))
+  };
+
   return (
     <section>
       <h2 className="comp-title" id="contact">
@@ -32,7 +49,10 @@ function Contact() {
               /LoisBonsignore
             </p>
           </a>
-          <a href="https://www.linkedin.com/in/lo%C3%AFs-bonsignore/" className="contact-info">
+          <a
+            href="https://www.linkedin.com/in/lo%C3%AFs-bonsignore/"
+            className="contact-info"
+          >
             <p className="links-option">
               <img src={linkedin} alt="linkedin" className="svg-logo" /> Loïs
               Bonsignore
@@ -42,18 +62,47 @@ function Contact() {
             <p>Télechargez mon CV</p>
           </a>
         </div>
-        <div className="contact-form-container">
-          <label for="name">Nom</label>
-          <input id="name" type="text" />
-          <label for="firstname">Prénom</label>
-          <input id="firstname" type="text" />
-          <label for="mail">Adresse email</label>
-          <input id="mail" type="text" />
-          <label for="message">Message</label>
-          <textarea id="message" cols="30" rows="10"></textarea>
-          <button>Envoyer</button>
+          <form className="contact-form-container" onSubmit={(e) => handleSubmit(e)}>
+            <label htmlFor="lastname">Nom</label>
+            <input
+              id="lastname"
+              type="text"
+              name="lastname"
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, lastname: e.target.value })
+              }
+            />
+            <label htmlFor="firstname">Prénom</label>
+            <input
+              id="firstname"
+              type="text"
+              name="firstname"
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, firstname: e.target.value })
+              }
+            />
+            <label htmlFor="email">Adresse email</label>
+            <input
+              id="email"
+              type="text"
+              name="email"
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, email: e.target.value })
+              }
+            />
+            <label htmlFor="message">Message</label>
+            <textarea
+              id="message"
+              name="message"
+              cols="30"
+              rows="10"
+              onChange={(e) =>
+                setContactInfo({ ...contactInfo, message: e.target.value })
+              }
+            ></textarea>
+            <button type="submit">Envoyer</button>
+          </form>
         </div>
-      </div>
     </section>
   );
 }
